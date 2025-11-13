d55783ef36c79da0a5d253693ab238eafb40e75f
Tori <tori@alaniz.tech>
2025-11-13 17:47:58 -0600

    Added Client

--- CDOWin/Models/Reminders.cs
+++ CDOWin/Models/Reminders.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
 
 namespace CDOWin.Models {
     public record class Reminder(

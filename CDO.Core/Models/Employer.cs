eac6a96fbeb02939ca24e8eb877c7a814d317605
Jon <me@jonalaiz.com>
2025-12-10 11:40:14 -0600

    Added computed properties

--- CDO.Core/Models/Employer.cs
+++ CDO.Core/Models/Employer.cs
@@ -16,4 +16,25 @@ public record class Employer(
     string? supervisor,
     string? supervisorPhone,
     string? supervisorEmail
-    );
+    ) {
+    public string formattedAddress {
+        get {
+            if (address1 == null && address2 == null)
+                return "No address on file.";
+            else if (address2 == null) {
+                return $"{address1}\n{formattedCityStateZip}";
+            } else {
+                return $"{address1} {address2}\n{formattedCityStateZip}";
+            }
+        }
+    }
+
+    public string formattedCityStateZip {
+        get {
+            if (zip != null)
+                return $"{city}, {state} {zip}";
+            else
+                return $"{city}, {state}";
+        }
+    }
+}

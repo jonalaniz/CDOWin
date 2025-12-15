f5116542b69785ceb5ee780387bd8aecee785647
Jon <me@jonalaniz.com>
2025-12-15 15:37:45 -0600

    Created Update ViewModel

---
+++ CDOWin/ViewModels/CounselorUpdateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CDO.Core.DTOs;
+using CDO.Core.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDOWin.ViewModels;
+
+public partial class CounselorUpdateViewModel : ObservableObject {
+    public Counselor Original;
+    public UpdateCounselorDTO Updated = new UpdateCounselorDTO();
+
+    public CounselorUpdateViewModel(Counselor counselor) {
+        Original = counselor;
+    }
+}

bd7096b03d2d168e5cf827885c39e0c24b8edfd7
Victoria Alaniz <tori@alaniz.tech>
2025-12-15 13:11:05 -0800

    Made Service Authorization Search Bar

--- CDOWin/ViewModels/ServiceAuthorizationsViewModel.cs
+++ CDOWin/ViewModels/ServiceAuthorizationsViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.Media.Capture.Core;
 
 namespace CDOWin.ViewModels;
 
@@ -12,32 +13,57 @@ public partial class ServiceAuthorizationsViewModel : ObservableObject {
     private readonly IServiceAuthorizationService _service;
 
     [ObservableProperty]
-    public partial ObservableCollection<ServiceAuthorization> ServiceAuthorizations { get; private set; } = [];
+    public partial ObservableCollection<ServiceAuthorization> All { get; private set; } = [];
 
     [ObservableProperty]
-    public partial ObservableCollection<ServiceAuthorization> FilteredServiceAuthorizations { get; private set; } = [];
+    public partial ObservableCollection<ServiceAuthorization> Filtered { get; private set; } = [];
 
     [ObservableProperty]
-    public partial ServiceAuthorization? SelectedServiceAuthorization { get; set; }
+    public partial ServiceAuthorization? Selected { get; set; }
+
+    [ObservableProperty]
+    public partial string SearchQuery { get; set; } = string.Empty;
 
     public ServiceAuthorizationsViewModel(IServiceAuthorizationService service) {
         _service = service;
     }
 
+    partial void OnSearchQueryChanged(string value) {
+        ApplyFilter();
+    }
+
+    void ApplyFilter() {
+        if (string.IsNullOrWhiteSpace(SearchQuery)) {
+            Filtered = new ObservableCollection<ServiceAuthorization>(All);
+            return;
+        }
+
+        var query = SearchQuery.Trim().ToLower();
+        var result = All.Where(s =>
+        (s.client?.name?.ToLower().Contains(query) ?? false)
+        || (s.client?.counselorReference?.name?.ToLower().Contains(query) ?? false)
+        || (s.id?.ToLower().Contains(query) ?? false)
+        );
+
+        Filtered = new ObservableCollection<ServiceAuthorization>(result);
+    }
+
     public async Task LoadServiceAuthorizationsAsync() {
         var serviceAuthorizations = await _service.GetAllServiceAuthorizationsAsync();
         List<ServiceAuthorization> SortedServiceAuthorizations = serviceAuthorizations.OrderBy(o => o.clientID).ToList();
-        ServiceAuthorizations.Clear();
+        All.Clear();
 
         foreach (var serviceAuthorization in SortedServiceAuthorizations) {
-            ServiceAuthorizations.Add(serviceAuthorization);
+            All.Add(serviceAuthorization);
         }
+
+        ApplyFilter();
     }
 
     public async Task RefreshSelectedServiceAuthorization(string id) {
         var po = await _service.GetServiceAuthorizationAsync(id);
-        var index = ServiceAuthorizations.IndexOf(ServiceAuthorizations.First(p => p.id == id));
-        ServiceAuthorizations[index] = po;
-        SelectedServiceAuthorization = po;
+        var index = All.IndexOf(All.First(p => p.id == id));
+        All[index] = po;
+        Selected = po;
     }
 }

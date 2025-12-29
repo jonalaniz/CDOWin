5618264bed4b08a4a6ae513038e8bf62ccc5c0e6
Victoria Alaniz <tori@alaniz.tech>
2025-12-28 21:27:10 -0800

    Added create employer flyout menu for state selection

--- CDOWin/Views/Employers/Dialogs/CreateEmployer.xaml.cs
+++ CDOWin/Views/Employers/Dialogs/CreateEmployer.xaml.cs
@@ -1,8 +1,12 @@
+using CDO.Core.Models;
 using CDOWin.Controls;
 using CDOWin.Extensions;
+using CDOWin.Services;
 using CDOWin.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CDOWin.Views.Employers.Dialogs;
 
@@ -12,19 +16,47 @@ public sealed partial class CreateEmployer : Page {
     // Dependencies
     // =========================
     private readonly CreateEmployerViewModel ViewModel;
+    private List<State> _states;
 
     // =========================
     // Constructor
     // =========================
     public CreateEmployer(CreateEmployerViewModel viewModel) {
+        var states = AppServices.StatesViewModel.States.ToList();
+        _states = states;
         ViewModel = viewModel;
         DataContext = viewModel;
         InitializeComponent();
+        BuildStateDropdown();
+    }
+
+    private void BuildStateDropdown() {
+        var flyout = new MenuFlyout();
+
+        foreach (var state in _states) {
+            var item = new MenuFlyoutItem {
+                Text = state.shortName,
+                Tag = state.shortName
+            };
+
+            item.Click += StateSelected;
+            flyout.Items.Add(item);
+        }
+
+        StateDropDownButton.Flyout = flyout;
     }
 
     // =========================
     // Property Change Methods
-    // =========================
+    // ========================== 
+    private void StateSelected(object sender, RoutedEventArgs e) {
+        if (sender is MenuFlyoutItem item) {
+            var state = item.Tag.ToString();
+            ViewModel.State = state;
+            StateDropDownButton.Content = state;
+        }
+    }
+
     private void LabeledTextBox_TextChangedForwarded(object sender, TextChangedEventArgs e) {
         if (sender is not FrameworkElement fe || fe.Tag is not Field field)
             return;

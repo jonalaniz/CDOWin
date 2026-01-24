ac1e04fe6f37c1972630fd587b21b47d56f361b1
Jon <me@jonalaniz.com>
2026-01-24 13:07:39 -0600

    Updated interface to reflect new fields

---
+++ CDOWin/Converters/BoolVisibilityConverter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace CDOWin.Converters;
+public class BoolVisibilityConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, string language) {
+        if (value is bool b) return b == true ? Visibility.Visible : Visibility.Collapsed;
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+        throw new NotImplementedException();
+    }
+}

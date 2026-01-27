63e89a6f54f45f997fcdb12c5009021970a51495
Jon <me@jonalaniz.com>
2026-01-27 16:27:09 -0600

    GoTo Buttons now enable/disable based on if associated field is null

---
+++ CDOWin/Converters/NullEnabledConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace CDOWin.Converters;
+
+class NullToBoolConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, string language) {
+        return value == null ? false : true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+        throw new NotImplementedException();
+    }
+}

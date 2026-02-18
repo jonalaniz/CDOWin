f9768c19300d164780099855e6dec4a16a106fef
Jon <me@jonalaniz.com>
2026-02-18 03:16:48 -0600

    code cleanup

--- CDOWin/ViewModels/PlacementsViewModel.cs
+++ CDOWin/ViewModels/PlacementsViewModel.cs
@@ -8,7 +8,6 @@ using Microsoft.UI.Dispatching;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 

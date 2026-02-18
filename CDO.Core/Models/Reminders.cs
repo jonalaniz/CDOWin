364590ba6a8a0e99abbdb12b94be6b36901c1a79
Jon <me@jonalaniz.com>
2026-02-18 06:10:58 -0600

    Now we have no warnings

--- CDO.Core/Models/Reminders.cs
+++ CDO.Core/Models/Reminders.cs
@@ -4,7 +4,7 @@ public class Reminder {
     // Required creation fields
     public int Id { get; set; }
     public DateTime Date { get; set; }
-    public string Description { get; set; }
+    public required string Description { get; set; }
 
     // Optional fields
     public int ClientID { get; set; }

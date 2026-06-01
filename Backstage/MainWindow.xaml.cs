3f5e98f96e1827341b4a8798c186789ffce26989
Jon Alaniz <jon@alaniz.tech>
2026-06-01 03:00:23 -0500

    Added packages and updated windows

--- Backstage/MainWindow.xaml.cs
+++ Backstage/MainWindow.xaml.cs
@@ -1,28 +1,37 @@
+using CommunityToolkit.WinUI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.UI.ViewManagement;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Backstage;
 
-namespace Backstage {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
+public sealed partial class MainWindow : Window {
+
+    // =========================
+    // Constructor
+    // =========================
+    public MainWindow() {
+        InitializeComponent();
+        SetupWindow();
+    }
+
+    // =========================
+    // Window Setup
+    // =========================
+    private void SetupWindow() {
+        ExtendsContentIntoTitleBar = true;
+        //AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+
+        var manager = WinUIEx.WindowManager.Get(this);
+        manager.MinHeight = 800;
+        manager.MinWidth = 1200;
+        Tbar.Subtitle = GetAppVersion();
+    }
+
+    private string GetAppVersion() {
+        var version = Package.Current.Id.Version;
+        return $"Version {version.Major}.{version.Minor}.{version.Build}";
     }
 }

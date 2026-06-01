fa36166d53f28b863151099c5c1f9908df6fe8a2
Jon Alaniz <jon@alaniz.tech>
2026-06-01 18:00:02 -0500

    Added AdminClientSummary

---
+++ CDO.Core/DTOs/Admin/AdminClientSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDO.Core.DTOs.Admin; 
+public class AdminClientSummary {
+    // Non-optional fields
+    public int Id { get; init; }
+    public required bool Active { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required bool Ttw { get; init; }
+
+    // Nullable fields
+    public DateTime? CreatedAt { get; init; }
+    public DateTime? UpdatedAt { get; init; }
+    public DateTime? StartDate { get; init; }
+    public string? CaseID { get; init; }
+}

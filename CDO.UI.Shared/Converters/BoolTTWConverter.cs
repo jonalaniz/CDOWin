a13842dfe74f00a422d5e940cbaebd433eeced5b
Jon Alaniz <jon@alaniz.tech>
2026-06-19 02:54:31 -0500

    Added converter

--- CDO.UI.Shared/Converters/BoolTTWConverter.cs
+++ CDO.UI.Shared/Converters/BoolTTWConverter.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.UI.Xaml.Data;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CDO.UI.Shared.Converters;
 
-public partial class BoolActiveConverter : IValueConverter {
+public partial class BoolTTWConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, string language) {
         if (value is not bool b) return string.Empty;
-        return b == true ? "Mark Inactive..." : "Mark Active...";
+        return b == true ? "Unmark TTW" : "Mark TTW";
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, string language) {

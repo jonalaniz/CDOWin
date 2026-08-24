455695188d786e834a7e44a2ef57050f85f22557
Jon Alaniz <jon@alaniz.tech>
2026-08-24 18:24:35 -0500

    Updated styles and added notes detail

--- CDO.Core/DTOs/Admin/AdminClientNote.cs
+++ CDO.Core/DTOs/Admin/AdminClientNote.cs
@@ -10,6 +10,8 @@ public record class AdminClientNote(
     DateTime UpdatedAt
 ) {
     public string FormattedUpdatedTime => $"Updated at {UpdatedTime}";
+    public string FormattedUpdatedOnDate => $"Updated on {UpdatedDate}";
     public string LocalDate => Date.ToString(format: "MM/dd/yyyy");
     private string UpdatedTime => UpdatedAt.ToLocalTime().ToString(format: "hh:mm tt");
+    private string UpdatedDate => UpdatedAt.ToLocalTime().ToString(format: "MM/dd/yyyy hh:mm tt");
 }
